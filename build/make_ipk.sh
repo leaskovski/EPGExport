ba9d0329c587898d8e010dd89c27ca7d22a71ba9
Leaskovski <tristan.leask@googlemail.com>
2020-07-22 14:56:04 +0100

    Added build script to create IPK distribution file

---
+++ build/make_ipk.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+PACKAGE_NAME=enigma2-plugin-extensions-epgexport
+VERSION=1.0-r4
+INSTALL_LOCATION=/usr/lib/enigma2/python/Plugins/Extensions/EPGExport
+
+# Make the directory for putting all the files in for the IPK
+mkdir -p ${PACKAGE_NAME}/{control,data}
+
+# Create the folder structure and copy in source files for data.tar
+mkdir -p ${PACKAGE_NAME}/data/${INSTALL_LOCATION}
+cp -r ../src ./${PACKAGE_NAME}/data/${INSTALL_LOCATION}
+
+# Create the control file for control.tar
+cat > ./${PACKAGE_NAME}/control/control <<EOF
+Package: $PACKAGE_NAME
+Version: $VERSION
+Description: EPGExport Plugin
+Section: base
+Priority: optional
+Maintainer: Leaskovski
+Architecture: all
+OE: enigma2-plugins
+Homepage: https://github.com/leaskovski/EPGExport
+Depends: python-requests, python-lzma
+EOF
+
+# Create the prerm file for control.tar
+cat > ./${PACKAGE_NAME}/control/prerm <<EOF
+#!/bin/sh
+rm $INSTALL_LOCATION/*.pyo > /dev/null 2>&1
+rm $INSTALL_LOCATION/*.pyc > /dev/null 2>&1
+exit 0
+EOF
+
+# Create the debian-binary file
+cat > ./${PACKAGE_NAME}/debian-binary <<EOF
+2.0
+EOF
+
+# Package files
+pushd ./${PACKAGE_NAME}/control
+tar --numeric-owner --group=0 --owner=0 -czf ../control.tar.gz ./*
+popd
+pushd ./${PACKAGE_NAME}/data
+tar --numeric-owner --group=0 --owner=0 -czf ../data.tar.gz ./*
+popd
+pushd ./${PACKAGE_NAME}
+tar --numeric-owner --group=0 --owner=0 -cf ../${PACKAGE_NAME}_${VERSION}_all.ipk ./debian-binary ./data.tar.gz ./control.tar.gz 
+popd
+
+# Clean up
+rm -r ./${PACKAGE_NAME}
